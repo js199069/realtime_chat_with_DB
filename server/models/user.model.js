d46193330299d745e01314fb2fa7108c4bf5a0ac
jalilseif <jalilseif10@gmail.com>
2024-08-11 21:26:31 +0330

    remoe default imae url

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -4,7 +4,7 @@ const userSchema = mongoose.Schema({
     name: {type: String, required: true},
     email: { type:String, required: true, unique:true },
     password:{type: String, required: true},
-    pic:{type:String,required: true, default: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=580&q=80"}
+    pic:{type:String,required: true, default: ""}
 },{
     timestamps: true
 });
